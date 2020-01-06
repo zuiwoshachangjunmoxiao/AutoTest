e7eaf8aa1bb005266204f7de838e99a032f90444
zuiwoshachangjunmoxiao <smilegks@163.com>
2020-01-06 21:33:31 +0800

    提交-testng注解-代码

---
+++ Chapter5/src/main/java/com/course/com/BasicAnnotation.java
@@ -0,0 +1,45 @@
+package com.course.com;
+
+import org.testng.annotations.*;
+
+public class BasicAnnotation {
+    @Test
+    public void testCase01(){
+        System.out.println("testCase01 这是测试用例01");
+    }
+
+    @Test
+    public void testCase02(){
+        System.out.println("testCase02 这是测试用例02");
+    }
+
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("beforeMethod 这是在测试方法之前运行");
+    }
+
+    @AfterMethod
+    public void afterMethod(){
+        System.out.println("afterMethod 这是在测试方法之后运行");
+    }
+
+    @BeforeClass
+    public void beforeClass(){
+        System.out.println("beforeClass 这是在类之前运行");
+    }
+
+    @AfterClass
+    public void afterClass(){
+        System.out.println("afterClass 这是在类之后运行");
+    }
+
+    @BeforeSuite
+    public void beforeSuite(){
+        System.out.println("beforeSuite 这是在测试套件之前运行");
+    }
+
+    @AfterSuite
+    public void afterSuite(){
+        System.out.println("afterSuite 这是在测试套件之后运行");
+    }
+}
